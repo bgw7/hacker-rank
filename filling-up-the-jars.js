e35c46ad475d7dba9991230e7a28678af588038d
Benja <Benjamin.Wilson24@gmail.com>
2018-11-09 13:16:54 -0500

    refactor jar

--- filling-up-the-jars.js
+++ filling-up-the-jars.js
@@ -1,14 +1,26 @@
-var jar = 6;
-var cups = [1,2];
-
-var possible_ways_to_fill_jar;
-
-cups.forEach(c => {
-    if(jar%c === 0) {
-        possible_ways_to_fill_jar++;
+subsetSum = (numbers, target, partial, counter) => {
+    var s, n, remaining;
+    var counter = 0;
+    partial = partial || [];
+    sum = (num1, num2) => num1 + num2;
+  
+    s = partial.reduce(sum, 0);
+  
+    if (s === target) {
+        // console.log(partial) print or return combination used to get sum
+        return 1;
     }
-});
+  
+    if (s >= target) {
+      return 0;
+    }
+  
+    for (var i = 0; i < numbers.length; i++) {
+      n = numbers[i];
+      remaining = numbers.slice();
+      counter = counter + subsetSum(remaining, target, partial.concat([n]), counter);
+    }
+    return counter;
+  };
 
-for(var i = 0; i < jar;) {
-    
-}
+  console.log(subsetSum([1,2],6));  
