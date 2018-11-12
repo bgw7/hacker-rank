0aceae5673706342244d77e9f1415be262483147
Benja <Benjamin.Wilson24@gmail.com>
2018-11-12 15:05:48 -0500

    refac phrase parse

--- phrase-parse-and-count.js
+++ phrase-parse-and-count.js
@@ -1,16 +1,20 @@
 var phrase = 'red fish blue Fish';
-var parseAndCount = function(str) {
+
+var parseAndCount = str => {
  let wordArray = str.toLowerCase().split(" ");
  let results = {};
- for(let i = 0; i < wordArray.length; i++){ 
- if(wordArray[i] in results){
-results[wordArray[i]].count++;
- } else {
- results[wordArray[i]] = {count:1};
+
+ for(let i = 0; i < wordArray.length; i++) { 
+    if(wordArray[i] in results){
+        results[wordArray[i]].count++;
+    } else {
+        results[wordArray[i]] = {count:1};
+      }
  }
- }
+ 
  Object.keys(results).sort().forEach(key => {
- console.log(key, ' - ', results[key].count);
+    console.log(key, ' - ', results[key].count);
  });
 }
+
 parseAndCount(phrase);
